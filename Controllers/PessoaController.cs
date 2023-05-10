3b604201e17de710c7a9e893fd3099c19c08ba96
EdmarSantosM <santosem4@hotmail.com>
2023-05-10 20:02:25 -0300

    Requisição do tipo POST
    Trabalhando com método POST ( para cadastrar ) informações na tabela.

--- Controllers/PessoaController.cs
+++ Controllers/PessoaController.cs
@@ -5,6 +5,8 @@ using Api_com_ASP.NET_Core.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
+using Api_com_ASP.NET_Core.Models; 
+
 // Criando  uma rota  que  traz o Hello world
 
 namespace Api_com_ASP.NET_Core.Controllers
@@ -13,8 +15,29 @@ namespace Api_com_ASP.NET_Core.Controllers
     [Controller]
     [Route("[controller]")]
 
-    public class PessoaController
+    public class PessoaController : ControllerBase
     {
+
+        //Criação de um obj do tipo DataContext para  ter acesso as  funções
+        private DataContext dc;
+
+        //Construtor
+        public PessoaController(DataContext context)
+        {
+                //Na chamada da classe 
+                this.dc = context; // que recebe do Data
+        }
+
+        [HttpPost("api")]
+        public async Task<ActionResult> cadastrar([FromBody]Pessoa p) //  assincrono quando recebe o "await"  está linha fica  parada executa as demais  depois que essa finalizar  sua  função
+        {
+            dc.pessoa.Add(p);  // p parametro pessoa ( Recebe as  caracteristicas - Id, Nome, Cidade, Idade) - retorno do Json
+            await dc.SaveChangesAsync();
+
+            return Created("Objeto pessoa",p);
+        }
+
+
         [HttpGet("oi")] // Get ( Exibe e/ou  Lista)
         public string oi() // Método Simples
         {
@@ -22,3 +45,4 @@ namespace Api_com_ASP.NET_Core.Controllers
         }
     }
 }
+
