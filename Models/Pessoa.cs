a1aa95de38a31f0ab929f603cb89cf1349baeed1
EdmarSantosM <santosem4@hotmail.com>
2023-05-10 01:04:50 -0300

    Remoção e Adição de classe no Projeto.
    Remoção  da classe WheatherForecast
    
    Criação da classe Pessoa
    Inclusão  dos atributos
    Inclusão da annotations

---
+++ Models/Pessoa.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Api_com_ASP.NET_Core.Models
+{
+    public class Pessoa
+    {
+        //criacao dos atributos + inclusão de  annotation
+        [Key]
+        public int Id{get; set;} // get obtem e/ou extrai a informação e  o set  envia a informação
+        public string Nome { get; set; }
+        public string Cidade { get; set; }
+        public int Idade { get; set; }
+    }
+}
