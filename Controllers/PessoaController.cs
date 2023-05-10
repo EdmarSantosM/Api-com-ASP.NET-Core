693396f2d6b4aacc7a7f4dd2333dbab930805d7a
EdmarSantosM <santosem4@hotmail.com>
2023-05-10 19:15:26 -0300

    Sobre as Rotas ( Controller )
    Pode-se vincular  a um controller  (rotas)  a páginas  específicas sendo que cada rota  é vinculado a estrutura de uma página ( Home, Sobre , etc ).
    
    Vinculado em um Banco de  Dados que efetua (  Listagem, inserções, atualizações, remoção e ordenação  de informações.

--- Controllers/PessoaController.cs
+++ Controllers/PessoaController.cs
@@ -4,6 +4,8 @@ using Api_com_ASP.NET_Core.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
+// Criando  uma rota  que  traz o Hello world
+
 namespace Api_com_ASP.NET_Core.Controllers
 {
 
@@ -15,7 +17,7 @@ namespace Api_com_ASP.NET_Core.Controllers
         [HttpGet("oi")]
         public string oi()
         {
-            return "Hello World";
+            return "Hello World"; 
         }
     }
 }
