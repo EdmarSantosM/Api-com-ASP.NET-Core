509b8d5369548dbd1e0c9d536a011771ae06fcb9
EdmarSantosM <santosem4@hotmail.com>
2023-05-10 01:19:03 -0300

    Update DataContext.cs
    update

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,9 +1,13 @@
 using  Api_com_ASP.NET_Core.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Api_com_ASP.NET_Core.Data
 {
-    public class DataContext
+    public class DataContext : DbContext  //Herança
     {
-        
+        //Criação de um  método construtor
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {}
     }
 }
